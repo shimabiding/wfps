7f795577388a098520540a60f14e79f685ecd5b1
AS <86251587+natsubashi@users.noreply.github.com>
2024-11-10 09:04:15 +0900

    Add files via upload

---
+++ form3.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponetModel;
+
+namespace pspgf
+{
+    public partial class Form3 : Form{
+        public Form3{
+            this.ClientSize = new System.Drawing.Size(400, 300);
+        }
+    }
+}
